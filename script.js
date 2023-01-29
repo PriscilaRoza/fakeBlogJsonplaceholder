157040f512abce6aa72a1de22c80f9e64f5c0eb2
Priscila Roza <priscilarozasilva@gmail.com>
2023-01-29 11:21:39 -0300

    criando uma requisição assincrona

---
+++ script.js
@@ -0,0 +1,21 @@
+//https://jsonplaceholder.typicode.com/posts
+
+async function readPosts() {
+    let postArea = document.querySelector('.posts');
+    postArea.innerHTML = '<img src="https://cdn.dribbble.com/users/563824/screenshots/3633228/media/b620ccb3ae8c14ea5447d159ebb1da58.gif" >';
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+
+    if (json.length > 0) {
+        postArea.innerHTML = '';
+
+        for (let i in json) {
+            let postHtml = `<div><h2>${json[i].title}</h2>${json[i].body}<hr/></div>`;
+            postArea.innerHTML += postHtml;
+        }
+    } else {
+        postArea.innerHTML = 'Nenhum post para exibir';
+    }
+}
+
+readPosts()
